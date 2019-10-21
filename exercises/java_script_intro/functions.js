2cd762e738d45424f9d9f306be0170c62f50b8b7
Elisa <slatonea@gmail.com>
2019-10-21 13:28:31 -0400

    class-site
    added reading response and javascript exercise

---
+++ exercises/java_script_intro/functions.js
@@ -0,0 +1,18 @@
+function toggle() {
+var box = document.getElementById("box");
+box.classList.toggle("move");
+}
+
+$('.marquee').marquee({
+	//duration in milliseconds of the marquee
+	duration: 15000,
+	//gap in pixels between the tickers
+	gap: 50,
+	//time in milliseconds before the marquee will start animating
+	delayBeforeStart: 0,
+	//'left' or 'right'
+	direction: 'left',
+	//true or false - should the marquee be duplicated to show an effect of continues flow
+	duplicated: true,
+  pauseOnHover: true
+});
